af1f92eb35f827cb104c1e563c82bcce92822d6c
1200609 <hua.fan@ly.com>
2020-03-07 18:49:28 +0800

    add sofaboot-sample-resteasy

---
+++ sofaboot-sample-resteasy/src/main/java/com/alipay/sofa/boot/examples/demo/configuration/JaxrsApplication.java
@@ -0,0 +1,16 @@
+package com.alipay.sofa.boot.examples.demo.configuration;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * JAX-RS application
+ *
+ * @author Fabio Carvalho (facarvalho@paypal.com or fabiocarvalho777@gmail.com)
+ */
+@Component
+@ApplicationPath("/sample-app/")
+public class JaxrsApplication extends Application {
+}
